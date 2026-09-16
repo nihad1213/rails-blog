c7f544f5d307dc34f564aa5c2c632efce0aba9f2
Nihad <nihad.nemetli@gmail.com>
2026-09-16 14:07:51 +0400

    add: new column added

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[8.1].define(version: 2026_09_16_094747) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
   end
